697574586da63bcf41c91bf6e5b8dbeee19a2940
Ronmi Ren <ronmi.ren@gmail.com>
2017-07-18 01:01:46 +0800

    better middleware implementation

--- session/middleware.go
+++ session/middleware.go
@@ -5,41 +5,33 @@ import (
 	"net/http"
 )
 
-// Middleware is something like http.ServeMux, but allocates a session right before executing your handler.
+// Middleware add session to context before running your handler/router
 type Middleware interface {
 	http.Handler
-	Handle(pattern string, handler http.Handler)
-	HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
 }
 
 // NewMiddleware creates a net/http based middleware.
 //
 // It allocates session instance before executing handler.
-func NewMiddleware(m *Manager, mux *http.ServeMux) Middleware {
-	if mux == nil {
-		mux = http.NewServeMux()
-	}
+func NewMiddleware(m *Manager, h http.Handler) Middleware {
 	return middleware{
-		manager:  m,
-		ServeMux: mux,
+		manager: m,
+		h:       h,
 	}
 }
 
 type middleware struct {
 	manager *Manager
-	*http.ServeMux
+	h       http.Handler
 }
 
 func (m middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler, pat := m.Handler(r)
 	req := r
-	if pat != "" {
-		sess, err := m.manager.Start(w, r)
-		if err == nil {
-			req = r.WithContext(context.WithValue(r.Context(), "session", sess))
-			w.Header().Set("Trailer", "Set-Cookie")
-		}
+	sess, err := m.manager.Start(w, r)
+	if err == nil {
+		req = r.WithContext(context.WithValue(r.Context(), "session", sess))
+		w.Header().Set("Trailer", "Set-Cookie")
 	}
 
-	handler.ServeHTTP(w, req)
+	m.h.ServeHTTP(w, req)
 }
