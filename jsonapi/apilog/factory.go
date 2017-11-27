6e3ab0b1237d9c1808622f25c8dec7bb14276cac
Ronmi Ren <ronmi.ren@gmail.com>
2017-11-27 14:41:18 +0800

    fix doc and add new helper

--- jsonapi/apilog/factory.go
+++ jsonapi/apilog/factory.go
@@ -10,7 +10,7 @@ import (
 // LogProvider defines what info a jsonapi logger can use
 type LogProvider func(r *http.Request, data interface{}, err error)
 
-// For wraps handler for logging purpose
+// Use wraps handler, uses LogProvider p for logging purpose
 func Use(p LogProvider) jsonapi.Middleware {
 	return jsonapi.Middleware(func(h jsonapi.Handler) jsonapi.Handler {
 		return jsonapi.Handler(func(
@@ -20,6 +20,24 @@ func Use(p LogProvider) jsonapi.Middleware {
 		) (interface{}, error) {
 			data, err := h(d, r, w)
 			p(r, data, err)
+
+			return data, err
+		})
+	})
+}
+
+// LogErr wraps handler, uses LogProvider p for logging purpose, but only for errors
+func LogErr(p LogProvider) jsonapi.Middleware {
+	return jsonapi.Middleware(func(h jsonapi.Handler) jsonapi.Handler {
+		return jsonapi.Handler(func(
+			d *json.Decoder,
+			r *http.Request,
+			w http.ResponseWriter,
+		) (interface{}, error) {
+			data, err := h(d, r, w)
+			if err != nil {
+				p(r, data, err)
+			}
 
 			return data, err
 		})
