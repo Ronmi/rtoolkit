409ab830606c8478cd72c70e86a63a978105c591
Ronmi Ren <ronmi.ren@gmail.com>
2017-12-25 15:42:22 +0800

    update helper

--- async/at_most.go
+++ async/at_most.go
@@ -1,17 +1,46 @@
 package async
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // OnceAtMost prevents running specified function too fast
+//
+// It guarantees to run the function as many times as you call and no more than once
+// within the duration.
 func OnceAtMost(dur time.Duration, f func() error) func() error {
+	lock := new(sync.Mutex)
+	last := time.Now().Add(0 - dur)
 	return func() error {
-		begin := time.Now().UnixNano()
-		ret := f()
-		dur -= time.Duration(time.Now().UnixNano() - begin)
-		if dur > 0 {
-			time.Sleep(dur)
+		lock.Lock()
+		defer lock.Unlock()
+		if d := time.Now().Sub(last); d <= dur {
+			time.Sleep(dur - d)
 		}
+		last = time.Now()
+		return f()
+	}
+}
 
-		return ret
+// OnceWithin is identical to OnceAtMost, but calls within duration are ignored
+func OnceWithin(dur time.Duration, f func() error) func() error {
+	lock := new(sync.RWMutex)
+	last := time.Now().Add(0 - dur)
+	return func() error {
+		lock.RLock()
+		if d := time.Now().Sub(last); d <= dur {
+			lock.RUnlock()
+			return nil
+		}
+		lock.RUnlock()
+
+		lock.Lock()
+		defer lock.Unlock()
+		if d := time.Now().Sub(last); d <= dur {
+			return nil
+		}
+		last = time.Now()
+		return f()
 	}
 }
--- async/at_most_test.go
+++ async/at_most_test.go
@@ -18,11 +18,50 @@ func TestOnceAtMost(t *testing.T) {
 	}
 
 	for _, f := range funcs {
+		f()
 		begin := time.Now().UnixNano()
 		f()
 		actual := time.Duration(time.Now().UnixNano() - begin)
 		if actual < expect {
 			t.Errorf("expect %dns, got %dns", expect, actual)
+		}
+	}
+}
+
+func TestOnceWithin(t *testing.T) {
+	expect := 50 * time.Millisecond
+	a := 0
+	funcs := []struct {
+		expect int
+		f      func() error
+	}{
+		{
+			expect: 2,
+			f: OnceWithin(expect, func() error {
+				a++
+				return nil
+			}),
+		},
+		{
+			expect: 4,
+			f: OnceWithin(expect, func() error {
+				a++
+				time.Sleep(100 * time.Millisecond)
+				return nil
+			}),
+		},
+	}
+
+	for _, c := range funcs {
+		a = 0
+		c.f()
+		c.f()
+		time.Sleep(expect)
+		c.f()
+		c.f()
+
+		if a != c.expect {
+			t.Errorf("expected %d, got %d", c.expect, a)
 		}
 	}
 }
