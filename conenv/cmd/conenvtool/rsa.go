13d90c54ba8456cec1d2729eddf15542542cf0d7
Ronmi Ren <ronmi.ren@gmail.com>
2018-04-10 16:51:15 +0800

    generate codes to embedded private key in your program

--- conenv/cmd/conenvtool/rsa.go
+++ conenv/cmd/conenvtool/rsa.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/gob"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -13,16 +15,16 @@ import (
 )
 
 const rsacode = `
-func loadPriv() (key *rsa.PrivateKey, err error) {
-	data, err := ioutil.ReadFile("key.priv")
+func loadRSAPriv() (key *rsa.PrivateKey, err error) {
+	data, err := ioutil.ReadFile("rsa.priv")
 	if err != nil {
 		return
 	}
 
 	return x509.ParsePKCS1PrivateKey(data)
 }
-func loadPub() (key *rsa.PublicKey, err error) {
-	data, err := ioutil.ReadFile("key.pub")
+func loadRSAPub() (key *rsa.PublicKey, err error) {
+	data, err := ioutil.ReadFile("rsa.pub")
 	if err != nil {
 		return
 	}
@@ -31,7 +33,19 @@ func loadPub() (key *rsa.PublicKey, err error) {
 }
 `
 
-func loadPriv(fn string) (key *rsa.PrivateKey, err error) {
+const rsaembbed = `
+var rsaPrivateKey *rsa.PrivateKey
+
+func init() {
+	rsaPrivateKey = &rsa.PrivateKey{}
+	data := %#v
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	dec.Decode(rsaPrivateKey)
+}
+`
+
+func loadRSAPriv(fn string) (key *rsa.PrivateKey, err error) {
 	data, err := ioutil.ReadFile(fn)
 	if err != nil {
 		return
@@ -40,7 +54,7 @@ func loadPriv(fn string) (key *rsa.PrivateKey, err error) {
 	return x509.ParsePKCS1PrivateKey(data)
 }
 
-func loadPub(fn string) (key *rsa.PublicKey, err error) {
+func loadRSAPub(fn string) (key *rsa.PublicKey, err error) {
 	data, err := ioutil.ReadFile(fn)
 	if err != nil {
 		return
@@ -50,8 +64,12 @@ func loadPub(fn string) (key *rsa.PublicKey, err error) {
 }
 
 func genrsa() {
-	var bitSize int
+	var (
+		bitSize int
+		embed   bool
+	)
 	flag.IntVar(&bitSize, "size", 2048, "key size in bit")
+	flag.BoolVar(&embed, "embed", false, "generate codes to embed private key in your program instead")
 	flag.Parse()
 
 	log.Print("Generating key pair...")
@@ -63,18 +81,25 @@ func genrsa() {
 	priv := x509.MarshalPKCS1PrivateKey(key)
 	pub := x509.MarshalPKCS1PublicKey(&key.PublicKey)
 
-	log.Print("Writing public key to pubkey.pub...")
-	if err := ioutil.WriteFile("key.pub", pub, 0600); err != nil {
+	log.Print("Writing public key to rsa.pub...")
+	if err := ioutil.WriteFile("rsa.pub", pub, 0600); err != nil {
 		log.Fatalf("unexpected error: %s", err)
 	}
 
-	log.Print("Writing private key to key.priv...")
-	if err := ioutil.WriteFile("key.priv", priv, 0600); err != nil {
+	log.Print("Writing private key to rsa.priv...")
+	if err := ioutil.WriteFile("rsa.priv", priv, 0600); err != nil {
 		log.Fatalf("unexpected error: %s", err)
 	}
 
 	log.Print("Here's example code:")
-	fmt.Println(rsacode)
+	code := rsacode
+	if embed {
+		buf := &bytes.Buffer{}
+		enc := gob.NewEncoder(buf)
+		enc.Encode(priv)
+		code = fmt.Sprintf(rsaembbed, buf.Bytes())
+	}
+	fmt.Println(code)
 }
 
 func rsaenc() {
@@ -82,7 +107,7 @@ func rsaenc() {
 		keyfile string
 		data    string
 	)
-	flag.StringVar(&keyfile, "k", "key.pub", "public key file.")
+	flag.StringVar(&keyfile, "k", "rsa.pub", "public key file.")
 	flag.StringVar(&data, "data", "", "data to be encrypted (required).")
 	flag.Parse()
 
@@ -92,7 +117,7 @@ func rsaenc() {
 	}
 
 	log.Printf("Loading public key from %s...", keyfile)
-	key, err := loadPub(keyfile)
+	key, err := loadRSAPub(keyfile)
 	if err != nil {
 		log.Fatalf("unexpected error: %s", err)
 	}
@@ -112,7 +137,7 @@ func rsadec() {
 		keyfile string
 		data    string
 	)
-	flag.StringVar(&keyfile, "k", "key.priv", "private key file.")
+	flag.StringVar(&keyfile, "k", "rsa.priv", "private key file.")
 	flag.StringVar(&data, "data", "", "cipher text to be decrypted (required).")
 	flag.Parse()
 
@@ -122,7 +147,7 @@ func rsadec() {
 	}
 
 	log.Printf("Loading private key from %s...", keyfile)
-	key, err := loadPriv(keyfile)
+	key, err := loadRSAPriv(keyfile)
 	if err != nil {
 		log.Fatalf("unexpected error: %s", err)
 	}
