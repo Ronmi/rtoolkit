0ea7223b3b8ce6f6669b785c212efb3ed6459b68
Ronmi Ren <ronmi.ren@gmail.com>
2018-04-10 19:35:14 +0800

    add best parctice example

--- conenv/parser_test.go
+++ conenv/parser_test.go
@@ -1,6 +1,7 @@
 package conenv
 
 import (
+	"crypto/dsa"
 	"fmt"
 	"os"
 	"reflect"
@@ -139,5 +140,54 @@ func ExampleParser_ParseWithPrefix() {
 
 	fmt.Printf("%+v", c)
 
+	// output: {Master:{Host:192.168.0.1 Port:13306 User:user Pass:pass} Slave:{Host:192.168.0.2 Port:3306 User:user Pass:pass}}
+}
+
+var testDSAPubKey *dsa.PublicKey
+
+func init() {
+	testDSAPubKey = &testDSAKey.PublicKey
+}
+
+func Example() {
+	// simulating your setup in container
+	os.Setenv("MYSQL_MASTER_HOST", "192.168.0.1")
+	os.Setenv("MYSQL_MASTER_PORT", "33fa")
+	os.Setenv("MYSQL_MASTER_USER", "user")
+	os.Setenv("MYSQL_MASTER_PASS", "ML7b/Db8fo0tuzmsgONZPjc7chzFa3wXTSViPyDa12X8inaHqvzgEigyqBAyR/rmL/69c+eCOJqs5eiasp4TF1pLYBYyp+4KWcizwqsioWgiSU5cxd4I7LxYg1UOItmsPCHshzP0lOZo4hDHn+GoiVDuRFkuPIhYhO9Co6g54z7aolHAcUT5JgVkNpfENFxiSTBBLxQVCQEoJcT29RXC7u1tmQ7drQTH9Dz0lDDMCGqugFz1k6FzBOOuzqT5mnYEniqJFTagdOHs2DU/Rv+35ZeU1vIqXvYor4U8yMm1S2mBvMRgXBE/AVeip1EY7Y8eewra7UFt5UXiW0cMvdHsfA==")
+	os.Setenv("MYSQL_MASTER_PASS_SIGN", "Cv+BBQEC/4QAAAAl/4IAIQJ1gEUv23YL5j5YkuS1Tzf5fcTcR/C8Q/QFWaUjIb1A5CX/ggAhArfRGm5HDHWPGnsnSareByKudJ6MWp+mhrzlOXJjVhXa")
+	os.Setenv("MYSQL_SLAVE_HOST", "192.168.0.2")
+	os.Setenv("MYSQL_SLAVE_USER", "user")
+	os.Setenv("MYSQL_SLAVE_PASS", "SKSG78sOMHr+7k6vIvM+/BEb41v0QYWtVnvoCh/iikEs9bKU6859l0m2XjSkKVxSlGy3Wo8sx/rXNl8FwWl/O29ori4JAep1bM/kPCXknOFe8HmPeqWNBk5TWc5zxnhKLXigds3Sqf06jkQ6/LqJgaUrdMccHWFTImHiLa5YX0svV86TRxpKEcZ/H/J+ALRaZFtDSs9JvPn8DpB3T96SWKp3bDX63hlf+F2ijJnk89kL6xB8uKBi9huNSuS9Vy2kdtIVdpLw6I7Wq3tpeAoGbSIay5h/i6kDtVUR5jQxeNZgtIFaWaIChoARgeDoIf4FttUwwmPNG/lzXzcWPfO1qA==")
+	os.Setenv("MYSQL_SLAVE_PASS_SIGN", "Cv+BBQEC/4QAAAAl/4IAIQKITi303J64pPQ99mDDkcaXhGx+L5iwvofJUrNp2MqljyX/ggAhAioAW3nUpvIXjpBfoxLWNgEEJ1b/3Sjag2Ql9fGsqJwm")
+	// Best practice is generating cipher tex and signatures for each variable
+	// separately, even if they are same value in fact.
+
+	// =========== real code begin here
+
+	// define config types
+	type dbconf struct {
+		Host string `env:"HOST"`
+		Port int    `env:"PORT,hex" envDefault:"0cea"`
+		User string `env:"USER"`
+		Pass string `env:"PASS,enc,required,sign"`
+	}
+	type conf struct {
+		Master dbconf `env:"MASTER_"`
+		Slave  dbconf `env:"SLAVE_"`
+	}
+
+	// load config from envvar
+	var c conf
+	p := DefaultParser()
+	p.Register("enc", RSAExtension(testRSAKey))
+	p.Register("sign", DSAExtension(testDSAPubKey))
+	if err := p.ParseWithPrefix(&c, "MYSQL_"); err != nil {
+		// handle error here
+	}
+	// =========== end of real code
+
+	// print values for package testing purpose
+	fmt.Printf("%+v", c)
 	// output: {Master:{Host:192.168.0.1 Port:13306 User:user Pass:pass} Slave:{Host:192.168.0.2 Port:3306 User:user Pass:pass}}
 }
